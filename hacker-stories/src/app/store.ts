fc68dd9d75602f56842d5e3521f9dae2f73b1899
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-27 21:22:47 +0100

    updated store.ts added storiesReducer, added: actions, appthunk

--- hacker-stories/src/app/store.ts
+++ hacker-stories/src/app/store.ts
@@ -1,11 +1,18 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const store = configureStore({
+	reducer: rootReducer
 });
+
+// if (process.env.NODE_ENV === "development" && module.hot) {
+// 	module.hot.accept("./rootReducer", () => {
+// 		const newRootReducer = require("./rootReducer").default;
+// 		store.replaceReducer(newRootReducer);
+// 	});
+// }
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+
+export default store;
