8917a8ffdc8296d01b55f595e7ba25c97d067905
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-28 21:11:07 +0100

    added routing to app

--- react-firebase-auth/src/app/App.tsx
+++ react-firebase-auth/src/app/App.tsx
@@ -1,51 +1,49 @@
 import React from "react";
-import { Counter } from "../features/counter/Counter";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+	Navigation,
+	LandingPage,
+	SignIn,
+	SignUp,
+	PasswordForget,
+	Home,
+	Account,
+	Admin
+} from "../components";
+import * as ROUTES from "../constants/routes";
+// import { Counter } from "../features/counter/Counter";
 import "./App.css";
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<Counter />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<span>
-					<span>Learn </span>
-					<a
-						className='App-link'
-						href='https://reactjs.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						React
-					</a>
-					<span>, </span>
-					<a
-						className='App-link'
-						href='https://redux.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						Redux
-					</a>
-					<span>, </span>
-					<a
-						className='App-link'
-						href='https://redux-toolkit.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						Redux Toolkit
-					</a>
-					,<span> and </span>
-					<a
-						className='App-link'
-						href='https://react-redux.js.org/'
-						target='_blank'
-						rel='noopener noreferrer'>
-						React Redux
-					</a>
-				</span>
-			</header>
-		</div>
+		<Router>
+			<Navigation />
+
+			<hr />
+			<Switch>
+				<Route exact path={ROUTES.LANDING}>
+					<LandingPage />
+				</Route>
+				<Route path={ROUTES.SIGN_UP}>
+					<SignUp />
+				</Route>
+				<Route path={ROUTES.SIGN_IN}>
+					<SignIn />
+				</Route>
+				<Route path={ROUTES.PASSWORD_FORGET}>
+					<PasswordForget />
+				</Route>
+				<Route path={ROUTES.HOME}>
+					<Home />
+				</Route>
+				<Route path={ROUTES.ACCOUNT}>
+					<Account />
+				</Route>
+				<Route path={ROUTES.ADMIN}>
+					<Admin />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
