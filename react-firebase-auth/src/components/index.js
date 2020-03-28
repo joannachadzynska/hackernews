901c6a80adba3b501f3c86126092a84ae89566d0
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-28 21:10:50 +0100

    imported components from folder components

---
+++ react-firebase-auth/src/components/index.js
@@ -0,0 +1,19 @@
+import Navigation from "./+Navigation";
+import LandingPage from "./+LandingPage";
+import SignUp from "./+SignUp";
+import SignIn from "./+SignIn";
+import PasswordForget from "./+PasswordForget";
+import Home from "./+Home";
+import Account from "./+Account";
+import Admin from "./+Admin";
+
+export {
+	Navigation,
+	LandingPage,
+	SignUp,
+	SignIn,
+	PasswordForget,
+	Home,
+	Account,
+	Admin
+};
