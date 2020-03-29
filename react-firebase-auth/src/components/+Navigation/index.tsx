13ef770a66252eeb55859e3961ded9b403246cbe
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-29 21:54:12 +0200

    updated navigation

--- react-firebase-auth/src/components/+Navigation/index.tsx
+++ react-firebase-auth/src/components/+Navigation/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-
 import NavigationAuth from "./NavigationAuth";
 import NavigationNonAuth from "./NavigationNonAuth";
+import { AuthUserContext } from "../+Session";
 
 export interface NavigationProps {
 	authUser: any;
 }
 
 const Navigation: React.SFC<NavigationProps> = ({ authUser }) => {
-	return <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>;
+	return (
+		<div>
+			<AuthUserContext.Consumer>
+				{(authUser) => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+			</AuthUserContext.Consumer>
+		</div>
+	);
 };
 
 export default Navigation;
