7f67571000d69d279102e021c2563bbb0067e4e2
Joanna Chądzyńska <jmalek87@gmail.com>
2020-03-31 17:18:56 +0200

    updated passwordchange component

--- react-firebase-auth/src/components/+PasswordChange/index.tsx
+++ react-firebase-auth/src/components/+PasswordChange/index.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FirebaseContext } from "../+Firebase";
+import PasswordChangeForm from "./PasswordChangeForm";
 
 export interface PasswordChangeProps {}
 
 const PasswordChange: React.SFC<PasswordChangeProps> = () => {
+	const firebase = useContext(FirebaseContext);
 	return (
 		<div>
 			<h1>Password change</h1>
+			<PasswordChangeForm firebase={firebase} />
 		</div>
 	);
 };
