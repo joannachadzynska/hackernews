3fb1899dfc5ffede97c149715d20f8e235dc0882
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-02 18:55:03 +0200

    added useSemiPersistentState hook

---
+++ react-firebase-auth/src/customHooks/SemiPersistentState.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, useRef } from "react";
+
+const useSemiPersistentState = (
+	key: string,
+	initialState: any
+): [string, (newValue: string) => void] => {
+	const isMounted = useRef(false);
+
+	const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+	useEffect(() => {
+		if (!isMounted.current) {
+			isMounted.current = true;
+		} else {
+			localStorage.setItem(key, value);
+		}
+	}, [value, key]);
+
+	return [value, setValue];
+};
+
+export default useSemiPersistentState;
