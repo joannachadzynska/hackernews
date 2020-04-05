b1f39c5eb6481cc083617ff20d9df65531e540a5
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-05 16:35:28 +0200

    deleted state and methods for loading users

--- react-firebase-auth/src/components/+Admin/index.tsx
+++ react-firebase-auth/src/components/+Admin/index.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState, useEffect } from "react";
-import { useHistory, Switch, Route, Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { useHistory, Switch, Route } from "react-router-dom";
 import { FirebaseContext } from "../+Firebase";
 // import { AuthUserContext } from "../+Session";
 import UsersList from "./UsersList";
@@ -12,29 +12,8 @@ export interface AdminProps {}
 const Admin: React.SFC<AdminProps> = () => {
 	const firebase = useContext(FirebaseContext);
 	// const authUser: any = useContext(AuthUserContext);
-	const [state, setState] = useState({
-		loading: false,
-		users: []
-	});
+
 	const history = useHistory();
-
-	useEffect(() => {
-		setState({
-			...state,
-			loading: true
-		});
-
-		const usersCollectionRef = firebase
-			.usersFirestore()
-			.onSnapshot((snapshot: any) => {
-				setState({
-					users: firebase.convertCollectionsSnapshotToMap(snapshot),
-					loading: false
-				});
-			});
-
-		return () => usersCollectionRef();
-	}, []);
 
 	const condition = (authUser: any) =>
 		authUser && authUser.roles.includes(ROLES.ADMIN);
@@ -55,8 +34,6 @@ const Admin: React.SFC<AdminProps> = () => {
 		<div>
 			<h1>Admin</h1>
 			<p>The Admin Page is accessible by every signed in admin user.</p>
-
-			{state.loading && <div>Loading...</div>}
 			<p>Restricted area! Only users with the admin role are authorized</p>
 
 			<Switch>
@@ -64,7 +41,7 @@ const Admin: React.SFC<AdminProps> = () => {
 					<UserDetails />
 				</Route>
 				<Route exact path={ROUTES.ADMIN}>
-					<UsersList users={state.users} />
+					<UsersList />
 				</Route>
 			</Switch>
 		</div>
