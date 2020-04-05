d385d4177a2d2ed3596d8487fc0d7e0b740a7781
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-05 16:13:42 +0200

    created user details page

---
+++ react-firebase-auth/src/components/+Admin/UserDetails.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Title } from "../+LandingPage/style";
+
+export interface UserDetailsProps {}
+
+const UserDetails: React.SFC<UserDetailsProps> = () => {
+	return (
+		<div>
+			<Title>User details page</Title>
+		</div>
+	);
+};
+
+export default UserDetails;
