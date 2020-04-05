3515cc5ac9c2b5a6fe71ab6246c8b303ba9f4258
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-05 16:36:17 +0200

    added state for fetching users

--- react-firebase-auth/src/components/+Admin/UsersList.tsx
+++ react-firebase-auth/src/components/+Admin/UsersList.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import User from "./User";
+import { FirebaseContext } from "../+Firebase";
 import { StyledList } from "./style";
 
-export interface UsersListProps {
-	users: any;
-}
+export interface UsersListProps {}
 
-const UsersList: React.SFC<UsersListProps> = React.memo(({ users }) => {
+const UsersList: React.SFC<UsersListProps> = React.memo(() => {
+	const firebase = useContext(FirebaseContext);
+	// const authUser: any = useContext(AuthUserContext);
+	const [state, setState] = useState({
+		loading: false,
+		users: []
+	});
+
+	const { loading, users } = state;
+	// const history = useHistory();
+
+	useEffect(() => {
+		setState({
+			...state,
+			loading: true
+		});
+
+		const usersCollectionRef = firebase
+			.usersFirestore()
+			.onSnapshot((snapshot: any) => {
+				setState({
+					users: firebase.convertCollectionsSnapshotToMap(snapshot),
+					loading: false
+				});
+			});
+
+		return () => usersCollectionRef();
+	}, []);
 	return (
-		<StyledList>
-			{users.map((user: any) => (
-				<User key={user.uid} user={user} />
-			))}
-		</StyledList>
+		<div>
+			<h2>Users</h2>
+			{loading && <div>Loading...</div>}
+			<StyledList>
+				{users.map((user: any) => (
+					<User key={user.uid} user={user} />
+				))}
+			</StyledList>
+		</div>
 	);
 });
 
