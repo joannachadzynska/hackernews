2896e32a7fadfa98d386aa14c767b0b93b525175
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-05 16:35:59 +0200

    added link to user details

--- react-firebase-auth/src/components/+Admin/User.tsx
+++ react-firebase-auth/src/components/+Admin/User.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 export interface UserProps {
 	user: any;
@@ -11,13 +13,16 @@ const User: React.SFC<UserProps> = ({ user }) => {
 			<span style={{ width: "40%" }}>
 				<strong>ID:</strong> {user.uid}
 			</span>
-			<span style={{ width: "30%" }}>
+			<span style={{ width: "25%" }}>
 				<strong>E-Mail:</strong> {user.data.email}
 			</span>
-			<span style={{ width: "30%" }}>
+			<span style={{ width: "25%" }}>
 				<strong>Username:</strong> {user.data.displayName}
 			</span>
-
+			{/* <span><strong>Created at: </strong></span> */}
+			<span style={{ width: "10%" }}>
+				<Link to={`${ROUTES.ADMIN}/users/${user.uid}`}>Details</Link>
+			</span>
 			{/* <span>
 				<strong>Role: </strong>
 			</span> */}
