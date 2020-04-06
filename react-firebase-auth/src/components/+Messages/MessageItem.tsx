ddf31d7a91684654b93e7a6e3dcf702de430be88
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-06 20:24:09 +0200

    added deleting function to message item, updated message text

--- react-firebase-auth/src/components/+Messages/MessageItem.tsx
+++ react-firebase-auth/src/components/+Messages/MessageItem.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Button } from "../shared";
 
 export interface MessageItemProps {
 	message: any;
+	onRemoveMessage: any;
 }
 
-const MessageItem: React.SFC<MessageItemProps> = ({ message }) => {
+const MessageItem: React.SFC<MessageItemProps> = ({
+	message,
+	onRemoveMessage
+}) => {
 	return (
 		<li>
-			<strong>{message.uid}</strong> {message.text}
+			<strong>{message.uid}</strong> {message.data.text}
+			<Button onClick={() => onRemoveMessage(message.uid)}>Delete</Button>
 		</li>
 	);
 };
