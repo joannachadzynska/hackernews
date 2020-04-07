24555da43cb582b1fd46c7d80ee6a9842326f5d2
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-07 21:36:28 +0200

    added utils.ts file for reusable methods

---
+++ react-firebase-auth/src/helpers/utils.ts
@@ -0,0 +1,19 @@
+import { format, fromUnixTime, formatDistance } from "date-fns";
+
+export const convertSecondsToDate = (timestamp: number) => {
+	if (timestamp === undefined) return;
+	const date = fromUnixTime(timestamp);
+	// const today = new Date();
+	// const distance = formatDistance(date, today);
+
+	return format(date, "eeee do MMMM yyyy, kk:mm:ss, OOOO");
+};
+
+export const convertMessageDate = (timestamp: number) => {
+	const date = fromUnixTime(timestamp);
+	const formatDate = format(date, "iii LLL y, k:m:s");
+	const today = new Date();
+	const distance = formatDistance(date, today);
+
+	return distance;
+};
