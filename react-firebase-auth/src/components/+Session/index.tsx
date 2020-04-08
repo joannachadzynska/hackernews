7215cbff1e5c0d7a793a7e39a631685b3fa5d54d
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-08 21:25:33 +0200

    deleted session folder, customhooks folder and firebase.ts and context

--- react-firebase-auth/src/components/+Session/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-
-export const AuthUserContext = React.createContext("");
-
-// import withAuthentication from "./withAuthentication";
-// import withAuthorization from "./withAuthorization";
