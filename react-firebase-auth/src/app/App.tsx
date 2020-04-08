41e201717989f5b63e6808fb351407094d4d09c2
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-08 21:29:41 +0200

    deleted firebase context

--- react-firebase-auth/src/app/App.tsx
+++ react-firebase-auth/src/app/App.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useContext } from "react";
-import { FirebaseContext } from "../components/+Firebase";
+import React, { useEffect } from "react";
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "../features/auth/authSlice";
+import {
+	createUserProfileDocument,
+	onAuthUserListener
+} from "../components/+Firebase/firebase.utils";
 import {
 	Navigation,
 	LandingPage,
@@ -13,82 +17,73 @@ import {
 	Account,
 	Admin
 } from "../components";
-import useSemiPersistentState from "../customHooks/SemiPersistentState";
-import { AuthUserContext } from "../components/+Session";
 import * as ROUTES from "../constants/routes";
 import "./App.css";
 
 const App = () => {
-	const [authUser, setAuthUser] = useSemiPersistentState("authUser", null);
-	const firebase = useContext(FirebaseContext);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const listener = firebase.onAuthUserListener(
+		const listener = onAuthUserListener(
 			(authUser: any) => {
 				if (authUser) {
 					const displayName = authUser.displayName;
-					firebase.createUserProfileDocument(authUser, {
+					createUserProfileDocument(authUser, {
 						displayName
 					});
 					// userRef.onSnapshot((snapShot: any) => {
 					// 	setAuthUser({ id: snapShot.id, ...snapShot.data() });
 					// });
 				}
-				localStorage.setItem("authUser", JSON.stringify(authUser));
-				setAuthUser(authUser);
+
 				dispatch(setCurrentUser(authUser));
 			},
 			() => {
-				setAuthUser("");
 				dispatch(setCurrentUser(""));
-				localStorage.removeItem("authUser");
 			}
 		);
 
 		return () => listener();
-	}, [firebase, setAuthUser]);
+	}, [dispatch]);
 
 	return (
-		<AuthUserContext.Provider value={authUser}>
-			<Router>
-				<header>
-					<Navigation />
-				</header>
+		<Router>
+			<header>
+				<Navigation />
+			</header>
 
-				<main className='wrapper'>
-					<Switch>
-						<Route exact path={ROUTES.LANDING}>
-							<LandingPage />
-						</Route>
-						<Route path={ROUTES.SIGN_UP}>
-							<SignUp />
-						</Route>
-						<Route path={ROUTES.SIGN_IN}>
-							<SignIn />
-						</Route>
-						<Route path={ROUTES.PASSWORD_FORGET}>
-							<PasswordForget />
-						</Route>
-						<Route path={ROUTES.HOME}>
-							<Home />
-						</Route>
-						<Route path={ROUTES.ACCOUNT}>
-							<Account />
-						</Route>
-						<Route path={ROUTES.ADMIN}>
-							<Admin />
-						</Route>
-					</Switch>
-				</main>
+			<main className='wrapper'>
+				<Switch>
+					<Route exact path={ROUTES.LANDING}>
+						<LandingPage />
+					</Route>
+					<Route path={ROUTES.SIGN_UP}>
+						<SignUp />
+					</Route>
+					<Route path={ROUTES.SIGN_IN}>
+						<SignIn />
+					</Route>
+					<Route path={ROUTES.PASSWORD_FORGET}>
+						<PasswordForget />
+					</Route>
+					<Route path={ROUTES.HOME}>
+						<Home />
+					</Route>
+					<Route path={ROUTES.ACCOUNT}>
+						<Account />
+					</Route>
+					<Route path={ROUTES.ADMIN}>
+						<Admin />
+					</Route>
+				</Switch>
+			</main>
 
-				<footer>
-					<span className='wrapper'>
-						© Joanna Chądzyńska {new Date().getFullYear()}
-					</span>
-				</footer>
-			</Router>
-		</AuthUserContext.Provider>
+			<footer>
+				<span className='wrapper'>
+					© Joanna Chądzyńska {new Date().getFullYear()}
+				</span>
+			</footer>
+		</Router>
 	);
 };
 
