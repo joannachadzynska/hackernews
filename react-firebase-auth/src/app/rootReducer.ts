a1d83f667e5181b04ce4fdde0bee62641dabf183
Joanna Chądzyńska <jmalek87@gmail.com>
2020-04-10 20:49:22 +0200

    added reducers to store: filter and todos

--- react-firebase-auth/src/app/rootReducer.ts
+++ react-firebase-auth/src/app/rootReducer.ts
@@ -5,17 +5,21 @@ import storage from "redux-persist/lib/storage";
 import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/users/usersSlice";
+import todosReducer from "../features/todos/todosSlice";
+import filtersReducer from "../features/filters/filtersSlice";
 
 const persistConfig = {
 	key: "root",
 	storage,
-	whitelist: ["auth"]
+	whitelist: ["auth"],
 };
 
 const rootReducer = combineReducers({
 	counter: counterReducer,
 	auth: authReducer,
-	users: usersReducer
+	users: usersReducer,
+	todos: todosReducer,
+	filters: filtersReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
